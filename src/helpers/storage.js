08ebc36d13ca0c07cebfce64d5f21de5ad3f8be9
Rajesh Sivanesan <rajesh.sivanesan@compass.com>
2022-06-15 01:18:09 +0530

    Storage files

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,1 +1,21 @@
+import secureLocalStorage from "react-secure-storage";
 
+/**
+ * Wrapper on react-secure-storage so that in future it can be replaced (plug & play strategy)
+ */
+const wrapper = {
+    setItem: (key, value) => {
+        secureLocalStorage.setItem(key, value);
+    },
+    getItem: (key) => {
+        return secureLocalStorage.getItem(key);
+    },
+    removeItem: (key) => {
+        secureLocalStorage.removeItem(key);
+    },
+    removeAllItems: () => {
+        secureLocalStorage.clear();
+    }
+}
+
+export default wrapper;
