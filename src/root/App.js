766f0108fcddd7f59accc413eecc4eb5f9da7957
Rajesh Sivanesan <rajesh.sivanesan@compass.com>
2022-06-15 01:19:25 +0530

    Root folder files

--- src/root/App.js
+++ src/root/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import './App.scss';
+import 'bootstrap';
 
+import logo from '../assets/img-qna.jpeg';
+
+import Header from './components/Header';
+import Questions from './components/Questions';
+import Footer from './components/Footer';
+
+/**
+ * Root component of the application
+ */
+function AppRoot() {
+  return (
+    <div className="App">
+      <img src={logo} className="logo" alt="logo" />
+      <div className='appWrapper'>
+        <Header />
+        <Questions />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default AppRoot;
